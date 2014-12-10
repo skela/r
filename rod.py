5c24f2b8cb6a10cdbcd00e6cc694f31a52d733c2
Alek Slater <slayte@gmail.com>
2014-12-11 01:49:47 +0800

    added ability to override the input and output folders by adding some magic to the Rodfile, added the beginnings of the cs proj updater

--- rod.py
+++ rod.py
@@ -29,6 +29,8 @@ class Rod(object):
         if os.path.exists(res):
             if os.path.isdir(res):
                 return res
+        if xcode_project_path is None:
+            return None
         name = os.path.basename(xcode_project_path)
         name = name.replace(".xcodeproj", "")
         res = os.path.join(folder_full_path, name, 'Images.xcassets')
@@ -42,7 +44,12 @@ class Rod(object):
         return None
 
     @staticmethod
-    def locate_image_resources_folder(xcode_project_path):
+    def locate_image_resources_output_folder(folder_path, xcode_project_path):
+
+        if xcode_project_path is None:
+            print "Failed to locate xcode project - i.e. Missing .xcodeproj file in folder %s\n(So Xcodeproject will not be updated, you have to manually add/remove image resources)" % folder_path
+            return folder_path
+
         name = os.path.basename(xcode_project_path)
         name = name.replace(".xcodeproj", "")
         root = os.path.split(xcode_project_path)[0]
@@ -100,6 +107,56 @@ class Rod(object):
         project.save()
 
     @staticmethod
+    def update_cs_project(cs_proj, img_folder):
+        print 'TODO: Add ability to read cs_proj files and update them based on the contents of img_folder'
+
+    @staticmethod
+    def read_rod_overrides():
+
+        d = {}
+
+        folder_path = os.curdir
+        if folder_path == '.':
+            folder_path = os.path.abspath(folder_path)
+        rod_file = os.path.join(folder_path, 'Rodfile')
+
+        f = open(rod_file, 'r')
+        lines = f.readlines()
+        f.close()
+
+        for line in lines:
+            if line.startswith('###'):
+                l = line[3:len(line)]
+                l = l.strip()
+                parts = l.split("=")
+                if len(parts) >= 2:
+                    k = parts[0].strip()
+                    v = parts[1].strip()
+                    d[k] = v
+        return d
+
+    @staticmethod
+    def override_rod_setting_if_exists(d, current, key, value_type):
+        if key in d:
+            val = d[key]
+            if value_type == 'path':
+                if val.startswith('.'):
+                    val = os.path.abspath(val)
+            return val
+        return current
+
+    @staticmethod
+    def read_projects(d, key):
+        cs = []
+        if key in d:
+            vals = d[key].split(',')
+            for val in vals:
+                if val.startswith('.'):
+                    val = os.path.abspath(val)
+                cs.append(val)
+        return cs
+
+    @staticmethod
     def init():
         folder_path = os.curdir
         if folder_path == '.':
@@ -117,16 +174,18 @@ class Rod(object):
             f = open(rod_file, 'w')
             f.write(c)
             f.close()
-            #os.system('touch "%s"' % rod_file)
+
             print '[*] Rodfile created successfully'
 
     @staticmethod
     def update():
-        (xcodeproj, img_folder, input_folder, assets_folder) = Rod.check(should_print_map=False)
+        (xcodeproj, img_folder, input_folder, assets_folder, cs_projects) = Rod.check(should_print_map=False)
         if input_folder is not None:
             Rod.regenerate_resources(input_folder, img_folder, assets_folder)
         if xcodeproj is not None:
             Rod.update_xcode_project(xcodeproj, img_folder)
+        for csproj in cs_projects:
+            Rod.update_cs_project(csproj, img_folder)
 
     @staticmethod
     def check(should_print_map):
@@ -134,23 +193,31 @@ class Rod(object):
         if folder_path == '.':
             folder_path = os.path.abspath(folder_path)
         xcodeproj = Rod.locate_xcodeproject_file(folder_path)
-        if xcodeproj is None:
-            print "Failed to locate xcode project - i.e. Missing .xcodeproj file in folder %s\n(So Xcodeproject will not be updated, you have to manually add/remove image resources)" % folder_path
-            img_folder = folder_path
-        else:
-            img_folder = Rod.locate_image_resources_folder(xcodeproj)
+        img_folder = Rod.locate_image_resources_output_folder(folder_path, xcodeproj)
         if img_folder is None:
             exit("Failed to locate xcode resources/images folder")
         input_folder = Rod.locate_input_resources_folder(folder_path)
         assets_folder = Rod.locate_image_assets_folder(folder_path, xcodeproj)
 
+        # Rod overrides
+        d = Rod.read_rod_overrides()
+        img_folder = Rod.override_rod_setting_if_exists(d, img_folder, 'OUTPUT', 'path')
+        input_folder = Rod.override_rod_setting_if_exists(d, input_folder, 'INPUT', 'path')
+
+        cs_projects = Rod.read_projects(d, 'CSPROJ')
+
         if should_print_map:
-            print '> Xcodeproj maps to %s' % xcodeproj
+            print '> xcodeproj maps to %s' % xcodeproj
             print '> Image Output folder maps to %s' % img_folder
             print '> Image Assets Output folder maps to %s' % assets_folder
             print '> Res Input folder maps to %s' % input_folder
 
-        return xcodeproj, img_folder, input_folder, assets_folder
+            if len(cs_projects) > 0:
+                print '> csproj maps to: '
+                for cs_proj in cs_projects:
+                    print '  %s' % cs_proj
+
+        return xcodeproj, img_folder, input_folder, assets_folder, cs_projects
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-i', '--init', help="Generate a Rodfile for the current directory.", action='store_true', default=False)
