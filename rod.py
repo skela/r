26c15e10099dde4542534908e65a17e0152578cf
Alek Slater <slayte@gmail.com>
2014-12-11 22:23:07 +0800

    rod now tries to update cs proj files

--- rod.py
+++ rod.py
@@ -5,6 +5,7 @@ import xmltodict
 
 from r import RiOS
 
+
 class Rod(object):
 
     @staticmethod
@@ -107,7 +108,95 @@ class Rod(object):
 
     @staticmethod
     def update_cs_project(cs_proj, img_folder):
-        print 'TODO: Add ability to read cs_proj files and update them based on the contents of img_folder'
+
+        def winshit_to_posix(p):
+            return p.replace('\\', '/')
+
+        def posix_to_winshit(p):
+            return p.replace('/', '\\')
+
+        def remove_winshit(p):
+            return p.replace("%40", "@")
+
+        def add_winshit(p):
+            return p.replace("@", "%40")
+
+        cs_folder = os.path.join(os.path.dirname(cs_proj))
+        rel_path = os.path.relpath(img_folder, cs_folder)  # '../iOSResources/Resources'
+
+        f = open(cs_proj)
+        dom = xmltodict.parse(f, strip_whitespace=True)
+        f.close()
+
+        item_groups = dom['Project']['ItemGroup']
+
+        folder_group = None
+        bundle_resource_group = None
+        for node in item_groups:
+            if 'Folder' in node:
+                folder_group = node["Folder"]
+            if 'BundleResource' in node:
+                bundle_resource_group = node["BundleResource"]
+
+        # Check for folder_group folder called Resources
+        if folder_group is None:
+            exit("Folder group not found - You need a Folder in the project tree called Resources for this to work")
+        else:
+            fres = None
+            for folder in folder_group:
+                fg = folder["@Include"]
+                if fg == "Resources\\":
+                    fres = fg
+                    break
+            if fres is None:
+                exit("'Resources' folder missing from project")
+
+        # Check for bundle resources group
+        if bundle_resource_group is None:
+            exit("Bundle resources group not found - You need at least one item in the Resources folder for the script to identify which is the correct item group")
+
+        existing_bundle_resources = {}
+        for bundle_resource in bundle_resource_group:
+            inc = bundle_resource["@Include"]
+            inc = winshit_to_posix(inc)
+            if inc.startswith(rel_path):
+                inc = remove_winshit(inc)
+                existing_bundle_resources[inc] = bundle_resource
+
+        missing_resources = []
+        files = os.listdir(img_folder)
+        for name in files:
+
+            if name == '.DS_Store':
+                continue
+            if name.endswith('.lproj'):
+                continue
+            if name.endswith('.xcassets'):
+                continue
+
+            file_path = os.path.join(rel_path, name)
+
+            if file_path not in existing_bundle_resources:
+                if not os.path.isdir(file_path):
+                    missing_resources.append(name)
+
+        missing_resource_dicts = []
+        for mrname in missing_resources:
+            mrname = add_winshit(mrname)
+            mrpath = posix_to_winshit(os.path.join(rel_path, mrname))
+            mrlink = posix_to_winshit(os.path.join('Resources', mrname))
+            od = xmltodict.OrderedDict()
+            od["@Include"] = mrpath
+            od["Link"] = mrlink
+            od["#text"] = ''
+            missing_resource_dicts.append(od)
+
+        for d in missing_resource_dicts:
+            bundle_resource_group.append(d)
+
+        xml = xmltodict.unparse(dom, pretty=True)
+        with open(cs_proj, 'w') as g:
+            g.write(xml)
 
     @staticmethod
     def read_rod_overrides():
@@ -229,7 +318,7 @@ class Rod(object):
                 for cs_proj in cs_projects:
                     print '  %s' % cs_proj
 
-        return xcodeproj, img_folder, input_folder, assets_folder, cs_projects
+        return xc_projects, img_folder, input_folder, assets_folder, cs_projects
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-i', '--init', help="Generate a Rodfile for the current directory.", action='store_true', default=False)
